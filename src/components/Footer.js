ab483e7a13d334faccfbf61d15755a5d611c55fa
unknown <benjaminjesukorede@gmail.com>
2025-05-12 12:30:30 +0100

    hello

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,8 +11,7 @@ const Footer = () => {
                   <FaHome size={20} style={{color:
                   "#fff", marginRight: "2rem" }} />
                   <div>
-                    <p>I Stay  At Ikorodu, Because I'm A Student At Laspotech </p>
-                    <p>First Gate, Ikorodu Lagos</p>
+                    <p>Lagos, Nigeria</p>
                   </div>
                 </div>
                   <div className="phone">
@@ -38,7 +37,10 @@ const Footer = () => {
 
             <div className="right">
               <h4>About Benjamin?</h4>
-              <p>I Study BackEnd Development At Webcity Lagos Ikeja,CEO Of Icoms Digital & A Student at Laspotech Ikorodu Lagos.</p>
+              <p>I believe I’m the best person for the job because I have a strong foundation in React and a deep understanding of building scalable and efficient web applications.
+                As an intermediate React programmer, I have hands-on experience with hooks, state management (such as Redux or Context API), and optimizing performance. 
+                I’m also a quick learner, constantly improving my skills by staying up-to-date with the latest React features and best practices. 
+                My problem-solving mindset and ability to work collaboratively make me a valuable asset to any development team.</p>
               <div>
                 <FaFacebook
                 size={30}
